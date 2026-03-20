24c3b7b2729b8ab0786860e356225efd19517599
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-03-20 09:35:55 -0300

    Update Main.java

--- POO/Abstracao_Encapsulamento/Main.java
+++ POO/Abstracao_Encapsulamento/Main.java
@@ -4,7 +4,11 @@ public class Main{
         Carro carro_1 = new Carro("Honda","Civic", 2017);
         carro_1.acelerar();
         carro_1.acelerar(200.20f);
-        System.out.println(carro_1.mostrarMarca());
-        carro_1.mostrarInfo();
+        System.out.println(carro_1.getMarca());
+        System.out.println(carro_1.getModelo());
+        carro_1.setAno(2018);   // valor válido
+        carro_1.setAno(2077);   // valor inválido
+        System.out.println(carro_1.getAno());
+        //carro_1.mostrarInfo();
     }
 }
