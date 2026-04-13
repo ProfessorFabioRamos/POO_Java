deb88151c9e3928834b0afd197791f5fd07bc010
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-04-13 08:57:55 -0300

    Update Terrestre.java

--- POO/Projeto_Veiculo/veiculos/terrestre/Terrestre.java
+++ POO/Projeto_Veiculo/veiculos/terrestre/Terrestre.java
@@ -1,5 +1,12 @@
 package veiculos.terrestre;
 
-public abstract class Terrestre {
-    
+import veiculos.Veiculo;
+
+public abstract class Terrestre extends Veiculo{
+    protected int numeroRodas;
+
+    public Terrestre(String nome,int numeroRodas){
+        super(nome);
+        this.numeroRodas = numeroRodas;
+    }
 }
