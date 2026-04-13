451c97b6204c46eadac703a282f3aeacedc328e3
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-04-13 09:22:53 -0300

    Create Carro.java

---
+++ POO/Projeto_Veiculo/veiculos/terrestre/Carro.java
@@ -0,0 +1,21 @@
+package veiculos.terrestre;
+
+public class Carro extends Terrestre{
+    protected String tipoCombustivel;
+
+    public Carro(String nome, int numeroRodas,String tipoCombustivel){
+        super(nome,numeroRodas);
+        this.tipoCombustivel = tipoCombustivel;
+    }
+
+    @Override // Método abstrato de Veículo, obrigação de implementar
+    public void mover() {
+        System.out.println(String.format("O Carro %s está se movendo com a velocidade %f km/h",this.nome,this.velocidade));
+    }
+
+    @Override
+    public void mostrarInfo(){
+        super.mostrarInfo();
+        System.out.println("Tipo de Combustível: "+this.tipoCombustivel);
+    }
+}
