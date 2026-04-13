0633c024527334e3a25679203fe3001d5d9c84a2
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-04-13 09:40:27 -0300

    Update Main.java

--- POO/Projeto_Veiculo/app/Main.java
+++ POO/Projeto_Veiculo/app/Main.java
@@ -1,10 +1,17 @@
 package app;
 
-import veiculos.Veiculo;
+import veiculos.terrestre.Carro;
 
 public class Main {
-    public static void main(String[]args){
-        // Não é permitido instanciar objeto da classe Veiculo
+    public static void main(String[] args){
+        // Não é possível instanciar objeto de classe abstrata
         //Veiculo veiculo_1 = new Veiculo("Fusca");
+
+        //Terrestre terrestre_1 = new Terrestre("Tanque de Guerra", 8);
+        Carro carro_1 = new Carro("Opala", 4, "Gasolina");
+        carro_1.acelerar(100);
+        carro_1.mover();
+        carro_1.desacelerar(20);
+        carro_1.mover();
     }
 }
