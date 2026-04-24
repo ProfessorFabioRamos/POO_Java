d6ad5af6f4f935301a26b7e25643e99e32335d04
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-04-24 09:17:51 -0300

    Update Main.java

--- POO/Projeto_Veiculo/app/Main.java
+++ POO/Projeto_Veiculo/app/Main.java
@@ -1,17 +1,37 @@
 package app;
 
 import veiculos.terrestre.Carro;
+import veiculos.aereo.JatoGuerra;
+
+//import veiculos.Veiculo;
+//import veiculos.terrestre.Terrestre;
 
 public class Main {
-    public static void main(String[] args){
-        // Não é possível instanciar objeto de classe abstrata
+    public static void main(String[]args){
+        // Não é permitido instanciar objeto da classe Veiculo
         //Veiculo veiculo_1 = new Veiculo("Fusca");
+        //Terrestre terrestre_1 = new Terrestre("Tanque");
 
-        //Terrestre terrestre_1 = new Terrestre("Tanque de Guerra", 8);
         Carro carro_1 = new Carro("Opala", 4, "Gasolina");
+
         carro_1.acelerar(100);
         carro_1.mover();
         carro_1.desacelerar(20);
         carro_1.mover();
+        carro_1.mostrarInfo();
+
+        Carro carro_2 = new Carro("BYD Dolphin", 4, "Elétrico");
+        carro_2.mostrarInfo();
+
+        JatoGuerra jato_1 = new JatoGuerra("F-16", 20);
+        jato_1.acelerar(2100);
+        jato_1.ganharAltitude(50000);
+        jato_1.mover();
+        for(int i=0; i < 10; i++){
+            jato_1.atirarMissel();
+        }
+        jato_1.perderAltitude(50000);
+        jato_1.desacelerar(2100);
+        jato_1.mostrarInfo();
     }
 }
