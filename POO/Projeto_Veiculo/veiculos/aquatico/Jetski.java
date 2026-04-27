0c2b0413a9760b03d5e9bc7db4b76395c86a91b8
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-04-27 10:36:13 -0300

    Update Jetski.java

--- POO/Projeto_Veiculo/veiculos/aquatico/Jetski.java
+++ POO/Projeto_Veiculo/veiculos/aquatico/Jetski.java
@@ -1,20 +1,22 @@
 package veiculos.aquatico;
 
 public class Jetski extends Aquatico{
-    protected int capacidadePessoas;
+    protected int tripulantes;
 
-    public Jetski(String nome, boolean possuiHelice, int capacidadePessoas){
-        super(nome, possuiHelice);
-        this.capacidadePessoas = capacidadePessoas;
+    public Jetski(String nome, boolean possuiHelice, int tripulantes){
+        super(nome,possuiHelice);
+        this.tripulantes = tripulantes;
     }
+
     @Override
     public void mover(){
-        System.out.println(String.format("O Jetski %s está se movendo na água com a velocidade %.2f km/h com %d pessoas à bordo.", nome, velocidade, capacidadePessoas));
+        System.out.println(String.format("O Jetski %s está se movendo na água com a velocidade %.2f km/h com %d pessoas à bordo.", nome, velocidade, tripulantes));
     }
+
     @Override
     public void mostrarInfo(){
         super.mostrarInfo();
-        System.out.println("Possui Hélice: "+ possuiHelice);
-        System.out.println("Pesoas à bordo: "+ capacidadePessoas);
+        System.out.println("Possui Hélice: "+possuiHelice);
+        System.out.println("Tripulantes: "+tripulantes);
     }
 }
