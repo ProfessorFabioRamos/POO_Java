d0e76e238a48e8139efd74315a009a5f3380d59d
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-05-15 09:40:22 -0300

    Create Main.java

---
+++ POO/Tratamento_Excecoes/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        //Divisao div = new Divisao(10, 0);
+        //div.dividir();
+
+        Idade idade = new Idade(2026);
+        idade.calcularIdade();
+    }
+}
