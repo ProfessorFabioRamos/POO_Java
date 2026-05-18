9effa0fe04a615ae27d4a49cdef8974370ff1ab0
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-05-18 10:39:42 -0300

    Update Main.java

--- POO/Tratamento_Excecoes/Main.java
+++ POO/Tratamento_Excecoes/Main.java
@@ -1,9 +1,18 @@
-public class Main {
+public class Main{
     public static void main(String[] args){
-        //Divisao div = new Divisao(10, 0);
-        //div.dividir();
+        //Divisao divisao = new Divisao(10, 0);
+        //divisao.dividir();
 
-        Idade idade = new Idade(2026);
-        idade.calcularIdade();
+        //Idade idade = new Idade(2026);
+        //idade.calcularIdade();
+
+        Apresentar apresentar = null;
+
+        try{
+            apresentar.apresentacao("Manuel");
+        }catch(NullPointerException e){
+            System.out.println("Erro crítico: Objeto ou atributo nulo");
+            System.out.println(e.getMessage());
+        }
     }
-}
+} 
