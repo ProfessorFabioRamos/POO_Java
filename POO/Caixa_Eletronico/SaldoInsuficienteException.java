ea4b20dbfb2da2021313e30a1e8f812a26874b07
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-05-22 09:05:14 -0300

    Update SaldoInsuficienteException.java

--- POO/Caixa_Eletronico/SaldoInsuficienteException.java
+++ POO/Caixa_Eletronico/SaldoInsuficienteException.java
@@ -1,6 +1,7 @@
 // Arquivo: SaldoInsuficienteException.java
 public class SaldoInsuficienteException extends Exception {
     public SaldoInsuficienteException(String mensagem) {
-        super(mensagem); // Repassa a mensagem para a classe mãe
+        // Repassa a mensagem para a classe super
+        super(mensagem); 
     }
 }
