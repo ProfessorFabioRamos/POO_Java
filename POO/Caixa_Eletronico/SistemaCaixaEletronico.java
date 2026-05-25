fd64cb3bc2a82fc3617b610fe55d3229b11fc276
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-05-25 10:13:06 -0300

    Update SistemaCaixaEletronico.java

--- POO/Caixa_Eletronico/SistemaCaixaEletronico.java
+++ POO/Caixa_Eletronico/SistemaCaixaEletronico.java
@@ -15,11 +15,38 @@ public class SistemaCaixaEletronico {
             // Operação 2: Sucesso de depósito
             conta.sacar(20.00);
             System.out.println("Saldo atual:R$"+conta.verificarSaldo());
+
+            // Operação 3: Vai gerar ValorInvalidoException com deposito
+            System.out.print("\nQuanto deseja depositar? R$ ");
+            double valorDeposito = scanner.nextDouble(); // Digitar valor menor que zero ou zero
+
+            // Operação 4: Vai gerar ValorInvalidoException com saque
+            System.out.print("\nQuanto deseja sacar? R$ ");
+            double valorSaque = scanner.nextDouble(); // Digitar valor menor que zero ou zero
+            conta.sacar(valorSaque);
+            
+            // Operação 5: Vai gerar SaldoInsuficienteException com saque
+            System.out.print("\nQuanto deseja sacar? R$ ");
+            valorSaque = scanner.nextDouble(); // Digitar valor maior que o saldo
+            conta.sacar(valorSaque);
+            System.out.println("Por favor, retire seu dinheiro.");
+            
         }catch(ValorInvalidoException e){
-
+            System.out.println("\n[ERRO DE NEGÓCIO]: " + e.getMessage());
+            System.out.println("Gostaria de fazer um empréstimo?"); // Ação amigável após o erro
         }
-        catch(SaldoInsuficienteException e){
-
+         } catch (ValorInvalidoException e) {
+            System.out.println("\n[ERRO DE VALIDAÇÃO]: " + e.getMessage());
+            
+        } catch (Exception e) {
+            // Erros não mapeados (como digitar uma letra no valor do saque)
+            System.out.println("\n[ERRO DO SISTEMA]: Ocorreu um problema inesperado. Contate o gerente.");
+            
+        } finally {
+            System.out.println("\n----------------------------------------");
+            System.out.println("Encerrando a sessão de " + conta.verificarSaldo() + " reais.");
+            System.out.println("Obrigado por utilizar nosso banco!");
+            scanner.close();
         }
     }
 }
