434a6e993f6b7ca845f60a79f261d75ddc321176
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-05-29 09:38:54 -0300

    Create Main.java

---
+++ POO/GUI_Swing/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+         // Instanciar o objeto TelaPedido
+         TelaPedido tela = new TelaPedido();
+         tela.setVisible(true);
+    }
+}
