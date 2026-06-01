fe9a8088e3cee6a5a4f126be678a791ecf9cfada
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-06-01 10:51:22 -0300

    Update TelaPedido.java

--- POO/GUI_Swing/TelaPedido.java
+++ POO/GUI_Swing/TelaPedido.java
@@ -1,32 +1,58 @@
-import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class TelaPedido extends JFrame{
+    private JTextField txtNome, txtSabor;
+    private JLabel labelResumo;
 
     public TelaPedido(){
-        // Configurações básicas da janela
-        setTitle("Pizzaria Vorcaro");
-        setSize(600,400);
+        // Configurações  basicas da janela
+        setTitle("Pizzaria Mario");
+        setSize(450,300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        setLayout(new BorderLayout(10,10));
 
-        // Criar painel e os componentes
-        JPanel painel = new JPanel();
-        JButton botaoVerificarPedido = new JButton("Verifica Pedido");
+        // Cria o painel e os componentes
+        JPanel painel = new JPanel(new GridLayout(2,2,5,5));
+        painel.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
 
-        botaoVerificarPedido.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e){
-                JOptionPane.showMessageDialog(null, "Seu pedido é uma pizza grande de pepperoni");
-            }
-        });
+        painel.add(new JLabel("Seu nome:"));
+        txtNome = new JTextField();
+        painel.add(txtNome);
 
-        // Adicionar os componentes
-        painel.add(botaoVerificarPedido);
-        add(painel);
+        painel.add(new JLabel("Sabor da Pizza"));
+        txtSabor = new JTextField();
+        painel.add(txtSabor);
+
+        JButton botaoConfirmar = new JButton("Confirmar Pedido");
+        botaoConfirmar.addActionListener(e -> processarPedido()); 
+
+        labelResumo = new JLabel("Aguardando pedido...",SwingConstants.CENTER);
+        labelResumo.setForeground(Color.BLUE);
+        labelResumo.setPreferredSize(new Dimension(0,50));
+
+        // Adicionar os elementos no Frame
+       add(painel, BorderLayout.CENTER);
+       add(botaoConfirmar, BorderLayout.NORTH);
+       add(labelResumo, BorderLayout.SOUTH);
+    }
+
+    public void processarPedido(){
+        // TO DO
     }
 }
