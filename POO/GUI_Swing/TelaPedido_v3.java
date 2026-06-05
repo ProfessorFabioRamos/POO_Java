e00d02e752157d949f6be8476b5aa4478c49ba3e
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-06-05 10:37:31 -0300

    Create TelaPedido_v3.java

---
+++ POO/GUI_Swing/TelaPedido_v3.java
@@ -0,0 +1,92 @@
+import javax.swing.JFrame;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
+import javax.swing.JComboBox; // Dropdown
+import javax.swing.JCheckBox;
+//import javax.swing.*;
+
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+//import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Label;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+
+public class TelaPedido extends JFrame{
+    private JTextField txtNome;
+    private JLabel labelResumo;
+    private JComboBox<String> cbSabor;
+    private JCheckBox checkBorda;
+
+    public TelaPedido(){
+        // Configurações básicas da janela
+        setTitle("Pizzaria Mario");
+        setSize(500,350);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout(10,10));
+
+        // Criar painel e os componentes
+        JPanel painel = new JPanel(new GridLayout(3,2,5,15));
+        painel.setBorder(BorderFactory.createEmptyBorder(20,20,10,20));
+
+        painel.add(new Label("Seu nome:"));
+        txtNome = new JTextField();
+        painel.add(txtNome);
+
+        // ComboBox Dropdown
+        painel.add(new Label("Sabor da pizza:"));
+        String[] opcoesSabor = {"Selecione um sabor", "Pepperoni","Calabresa","Marguerita","Frango com Catupiry"};
+        cbSabor = new JComboBox<>(opcoesSabor);
+        painel.add(cbSabor);
+
+        // Borda
+        // TO DO
+
+        JButton botaoConfirmar = new JButton("Confirmar Pedido");
+        botaoConfirmar.addActionListener(e -> processarPedido());
+
+        labelResumo = new JLabel("Aguardando pedido...", SwingConstants.CENTER);
+        labelResumo.setForeground(Color.BLUE);
+        labelResumo.setPreferredSize(new Dimension(0,50));
+        
+        // Adicionar o painel e elementos no frame
+        add(painel, BorderLayout.CENTER);
+        add(botaoConfirmar, BorderLayout.NORTH);
+        add(labelResumo, BorderLayout.SOUTH);
+    }
+
+    public void processarPedido(){
+        try{
+            String nome = txtNome.getText().trim();
+            //String sabor = txtSabor.getText().trim();
+
+            if(nome.isEmpty()){
+                throw new PedidoInvalidoException("Todos os campos devem ser preenchidos.");
+            }
+
+            // Sucesso
+            String msg = "Pedido de "+nome+": Pizza de ";
+            JOptionPane.showMessageDialog(this, "Pedido enviado com sucesso!");
+            labelResumo.setText("Último pedido: "+msg);
+        }catch(PedidoInvalidoException e){
+            JOptionPane.showMessageDialog(this, e.getMessage(), "Erro no pedido",JOptionPane.WARNING_MESSAGE);
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(this, e.getMessage(), "Erro nos dados",JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
+
+class PedidoInvalidoException extends Exception{
+    public PedidoInvalidoException(String msg){
+        super(msg);
+    }
+}
